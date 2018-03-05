450651ed1d66df15456a2a3ddeea19002b1dd27d
Guillermo Oliva Candido <33919349+gaoliva@users.noreply.github.com>
2018-03-05 03:03:19 -0500

    Button hide

--- assignments/lesson-6/scripts/hamburger.js
+++ assignments/lesson-6/scripts/hamburger.js
@@ -1,6 +1,5 @@
 function hamburger() {
     /*eslint-env browser*/
     //document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-    document.getElementById("navList").classList.toggle("hide")
-
+    document.getElementById("navList").classList.toggle("hide"); document.getElementById("hamburgerB").classList.toggle("hide");
 }
