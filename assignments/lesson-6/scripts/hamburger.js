00df9ba74221ad8452f9c7648bd8b37b1e95cf64
Guillermo Oliva Candido <33919349+gaoliva@users.noreply.github.com>
2018-03-05 00:27:42 -0500

    Hambuger Progress

--- assignments/lesson-6/scripts/hamburger.js
+++ assignments/lesson-6/scripts/hamburger.js
@@ -1,2 +1,5 @@
-/*eslint-env browser*/
-document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+function toggleNavMenu() {
+    /*eslint-env browser*/
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+    document.getElementsByClassName("hamburgerB")[0].classList.toggle("responsive");
+}
