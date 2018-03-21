292a550dfc8560fe84e1b7754a98950096218c2c
Guillermo Oliva Candido <33919349+gaoliva@users.noreply.github.com>
2018-03-20 20:57:31 -0400

    JSON updates L9

--- assignments/lesson-9/scripts/wu-weather.js
+++ assignments/lesson-9/scripts/wu-weather.js
@@ -1,60 +1,31 @@
-var section = document.querySelector('.gridCities');
-var requestURL = '//byui-cit230.github.io/weather/data/towndata.json';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-    var cityData = request.response;
-    showData(cityData);
-}
+var weatherObject = new XMLHttpRequest();
 
-function showData(jsonObj) {
-    var towns = jsonObj['towns'];
+weatherObject.open('GET', '//api.wunderground.com/api/77a2ddee5edb1323/conditions/q/MN/Franklin.json', true);
 
-    for (var i = 0; i < towns.length; i++) {
-        if (i === 2) { continue; }
-        var myArticle = document.createElement('article');
-        var myH2 = document.createElement('h2');
-        var myPara1 = document.createElement('p');
-        var myPara2 = document.createElement('p');
-        var myPara3 = document.createElement('p');
-        var myPara4 = document.createElement('p');
+weatherObject.send();
 
-        var myImage = document.createElement('IMG');
-        if (towns[i].name === "Franklin"){
-            myImage.setAttribute("src", "/assignments/lesson-9/images/franklin-cropped_L9.jpeg");
-            myImage.setAttribute("alt", towns[i].name);
-        }
-        else if (towns[i].name === "Greenville"){
-            myImage.setAttribute("src", "/assignments/lesson-9/images/greenville_original_L9_v2.jpeg");
-            myImage.setAttribute("alt", towns[i].name);
-        }
-        else if (towns[i].name === "Springfield"){
-            myImage.setAttribute("src", "/assignments/lesson-9/images/springfield_original_L9_v2.jpeg");
-            myImage.setAttribute("alt", towns[i].name);
-        }
+weatherObject.onload = function () {
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
 
+    document.getElementById('weatherCon').innerHTML = weatherInfo.current_observation.weather;
 
-        myH2.textContent = towns[i].name;
-        myPara1.textContent = towns[i].motto;
-        myPara2.textContent = "Founded: " + towns[i].yearFounded;
-        myPara3.textContent = "Population: " + towns[i].currentPopulation;
-        myPara4.textContent = "Average Rainfall: " + towns[i].averageRainfall;
+    //document.getElementById('place').innerHTML = weatherInfo.current_observation.display_location.full;
 
-        myArticle.appendChild(myH2);
-        myArticle.appendChild(myPara1);
-        myArticle.appendChild(myPara2);
-        myArticle.appendChild(myPara3);
-        myArticle.appendChild(myPara4);
-        myArticle.appendChild(myImage);
+    document.getElementById('weather_current').innerHTML = weatherInfo.current_observation.temp_f + "&deg;F" + " ";
+    document.getElementById('weather_prec').innerHTML = weatherInfo.current_observation.precip_today_in;
 
+    var icon_path = weatherInfo.current_observation.icon_url;
+    var urlString = document.location.href;
+    console.log(urlString);
+    var found = urlString.indexOf("https");
+    console.log(found);
 
-        section.appendChild(myArticle);
-
+    if (found >= 0) {
+        icon_path = icon_path.replace("http", "https")
     }
+
+    document.getElementById('weather_icon').src = weatherInfo.current_observation.icon_url;
+
 }
-
-
-
 
