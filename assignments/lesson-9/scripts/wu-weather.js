0ba19a08ee522185a74dfac3310f6316704b5617
Guillermo Oliva Candido <33919349+gaoliva@users.noreply.github.com>
2018-03-20 23:34:20 -0400

    Final changes L9

--- assignments/lesson-9/scripts/wu-weather.js
+++ assignments/lesson-9/scripts/wu-weather.js
@@ -1,31 +1,51 @@
-var weatherObject = new XMLHttpRequest();
+// Get current HTML page name (To be used for final project)
+var path = window.location.pathname;
+var page = path.split("/").pop();
+console.log(page);
 
-weatherObject.open('GET', '//api.wunderground.com/api/77a2ddee5edb1323/conditions/q/MN/Franklin.json', true);
+/****************************************
+ * Query data from the weather conditions
+ * page
+ ****************************************/
+//Initialize API request
+var weatherConditions = new XMLHttpRequest();
+weatherConditions.open('GET', '//api.wunderground.com/api/77a2ddee5edb1323/conditions/q/MN/Franklin.json', true);
+weatherConditions.send();
 
-weatherObject.send();
+// Get and display API data
+weatherConditions.onload = function () {
+    var conditionsInfo = JSON.parse(weatherConditions.responseText);
+    console.log(conditionsInfo);
 
-weatherObject.onload = function () {
-    var weatherInfo = JSON.parse(weatherObject.responseText);
-    console.log(weatherInfo);
-
-    document.getElementById('weatherCon').innerHTML = weatherInfo.current_observation.weather;
-
-    //document.getElementById('place').innerHTML = weatherInfo.current_observation.display_location.full;
-
-    document.getElementById('weather_current').innerHTML = weatherInfo.current_observation.temp_f + "&deg;F" + " ";
-    document.getElementById('weather_prec').innerHTML = weatherInfo.current_observation.precip_today_in;
-
-    var icon_path = weatherInfo.current_observation.icon_url;
-    var urlString = document.location.href;
-    console.log(urlString);
-    var found = urlString.indexOf("https");
-    console.log(found);
-
-    if (found >= 0) {
-        icon_path = icon_path.replace("http", "https")
-    }
-
-    document.getElementById('weather_icon').src = weatherInfo.current_observation.icon_url;
-
+    // Current weather condition e.g Overcast
+    document.getElementById('weather_cond').innerHTML = conditionsInfo.current_observation.weather;
+    // Current temperature in Fahrenheit
+    document.getElementById('weather_current').innerHTML = conditionsInfo.current_observation.temp_f + "&deg;F" + " ";
+    // Today's precipitation
+    document.getElementById('weather_prec').innerHTML = conditionsInfo.current_observation.precip_today_in + "%";
+    // Wind speed
+    document.getElementById('windSpeed_display').innerHTML = conditionsInfo.current_observation.wind_mph + " MPH" + " " + conditionsInfo.current_observation.wind_dir;
+    // Display weather icon with current condition
+    document.getElementById('weather_icon').src = conditionsInfo.current_observation.icon_url;
 }
 
+/****************************************
+ * Query data from the weather forecast
+ * page
+ ****************************************/
+var weatherForecast = new XMLHttpRequest();
+weatherForecast.open('GET', '//api.wunderground.com/api/77a2ddee5edb1323/forecast/q/MN/Franklin.json', true);
+weatherForecast.send();
+
+// Get and display API data
+weatherForecast.onload = function () {
+    var forecastInfo = JSON.parse(weatherForecast.responseText);
+    console.log(forecastInfo);
+
+    // High temp
+    document.getElementById('highTemp_display').innerHTML = forecastInfo.forecast.simpleforecast.forecastday["0"].high.fahrenheit + "&deg;F";
+    // Low temp
+    document.getElementById('lowTemp_display').innerHTML = forecastInfo.forecast.simpleforecast.forecastday["0"].low.fahrenheit + "&deg;F";
+    // Forecast text description
+    document.getElementById('weather_forecast').innerHTML = forecastInfo.forecast.txt_forecast.forecastday["0"].fcttext;
+}
